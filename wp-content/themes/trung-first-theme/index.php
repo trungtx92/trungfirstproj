1fcabce836b93e77231c356b56472969056bbb9c
rmit-s3626050-Trung-Pham <s3626050@student.rmit.edu.au>
2020-02-19 02:16:24 +1100

    loop post

--- wp-content/themes/trung-first-theme/index.php
+++ wp-content/themes/trung-first-theme/index.php
@@ -1,7 +1,13 @@
 <?php
     get_header();
 ?>
-this is the first theme
+<?php
+    while(have_post()){
+        the_post();
+?>
+    <h3><?php the_title();?></h3>
+    <?php the_content();?>
+    <?php } ?>
 <?php
     get_footer();
 ?>
