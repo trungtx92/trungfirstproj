34ef6d0d9e2f9f49bff7526a804f7c12c65a4ea8
rmit-s3626050-Trung-Pham <s3626050@student.rmit.edu.au>
2020-03-04 17:45:19 +1100

    m

--- wp-content/themes/trung-first-theme/front-page.php
+++ wp-content/themes/trung-first-theme/front-page.php
@@ -15,25 +15,26 @@
     <div class="full-width-split__one">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-        <div id="eventbrite-widget-container-96358935169"></div>
-        <script src="https://www.eventbrite.com.au/static/widgets/eb_widgets.js"></script>
-        <script type="text/javascript">
-            var exampleCallback = function() {
-                console.log('Order complete!');
-            };
-
-            window.EBWidgets.createWidget({
-                // Required
-                widgetType: 'checkout',
-                eventId: '96358935169',
-                iframeContainerId: 'eventbrite-widget-container-96358935169',
-
-                // Optional
-                iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
-                onOrderComplete: exampleCallback  // Method called when an order has successfully completed
-            });
-        </script>
-        
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month">Mar</span>
+            <span class="event-summary__day">25</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
+            <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month">Apr</span>
+            <span class="event-summary__day">02</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
+            <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
         <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
 
       </div>
@@ -41,28 +42,24 @@
     <div class="full-width-split__two">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-
-        <!-- Noscript content for added SEO -->
-<noscript><a href="https://www.eventbrite.com.au/e/bold-and-beautiful-tickets-96358935169" rel="noopener noreferrer" target="_blank"></noscript>
-<!-- You can customize this button any way you like -->
-<button id="eventbrite-widget-modal-trigger-96358935169" type="button">Buy Tickets</button>
-<noscript></a>Buy Tickets on Eventbrite</noscript>
-
-<script src="https://www.eventbrite.com.au/static/widgets/eb_widgets.js"></script>
-
-<script type="text/javascript">
-    var exampleCallback = function() {
-        console.log('Order complete!');
-    };
-
-    window.EBWidgets.createWidget({
-        widgetType: 'checkout',
-        eventId: '96358935169',
-        modal: true,
-        modalTriggerElementId: 'eventbrite-widget-modal-trigger-96358935169',
-        onOrderComplete: exampleCallback
-    });
-</script>
+        <?php
+          $homepagePosts = new WP_Query(array(
+            'posts_per_page' => 2
+          ));
+          while ($homepagePosts->have_posts()){
+            $homepagePosts->the_post();?>
+            <div class="event-summary">
+              <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                <span class="event-summary__month"><?php the_time('M');?></span>
+                <span class="event-summary__day"><?php the_time('d');?></span>  
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h5>
+                <p><?php echo wp_trim_words(get_the_content(), 20);?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+              </div>
+            </div>
+        <?php }
+        ?>
         
         <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
--- wp-content/themes/trung-first-theme/header.php
+++ wp-content/themes/trung-first-theme/header.php
@@ -13,6 +13,7 @@
         <nav class="main-navigation">
           <ul>
             <li><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
+            <li><a href="<?php echo site_url('/blog')?>">Blog</a></li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
